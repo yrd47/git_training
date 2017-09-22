68ffb9f80825232b66a8e6b5614d84e77f5252a5
ridanyuan <ridan.yuan@ele.me>
2017-09-22 16:42:46 +0800

    update log

--- Work_training/log/src/main/java/log/HelloWorld.java
+++ Work_training/log/src/main/java/log/HelloWorld.java
@@ -1,5 +1,6 @@
 package log;
 
+import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,11 @@ public class HelloWorld {
 
     public static void main(String[] args){
         Logger logger = LoggerFactory.getLogger(HelloWorld.class);
-        logger.info("Hello World");
+        BasicConfigurator.configure();
+        logger.error("Error Message");
+        logger.warn("Warn Message");
+        logger.info("Info Message");
+        logger.debug("Debug Message");
+        logger.trace("Trace Message");
     }
 }
